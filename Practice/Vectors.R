4938c26acfe044125f3f36d9be1d4e6f5e860956
Ehizeme <exa1967@g.rit.edu>
2016-03-25 04:54:58 -0400

    Praciting with vectors in R

---
+++ Practice/Vectors.R
@@ -0,0 +1,27 @@
+#
+# Creating vectors in R
+#
+
+# Create a vector with numbers 1 to 40
+vectorA <- c(1:40)
+
+# Get the third element
+vectorA[3]
+
+# Return the sequence of elements from 5 to 15
+vectorA[c(5:15)]
+
+# Return the 29th element
+vectorA[29]
+
+# Create a vector of characters
+charVector <- c("k", "j", "h", "a", "c", "m")
+
+# Return the third element
+charVector[3]
+
+# Return the first, third and fifth element
+charVector[c(1, 3, 5)]
+
+# Return the sequence of characters from 1 to 4
+charVector[c(1:4)]
